a613196b5182efbb1aee90ff64ba30059cb84a6d
fkayoungjo <Jordan.simon55@gmail.com>
2021-04-06 21:59:45 -0400

    call

--- frontend/components/VideoCall.jsx
+++ frontend/components/VideoCall.jsx
@@ -1,2 +1,37 @@
 import React from 'react';
 import { broadcastData, JOIN_CALL, LEAVE_CALL, EXCHANGE, ice } from './video_util.js';
+
+class VideoCall extends React.Component{
+  constructor(props){
+    super(props);
+    this.pcPeers = {};
+    this.userId = Math.floor(Math.random() * 10000);
+    this.joinCall = this.joinCall.bind(this);
+    this.leaveCall = this.leaveCall.bind(this);
+  }
+  componentDidMount(){
+  }
+  join(data){
+  }
+  joinCall(){
+  }
+  createPC(userId, offerBool){
+  }
+  exchange(data){
+  }
+
+  leaveCall(){
+  }
+  removeUser(data){
+  }
+  render(){
+    return(
+    <div className="VideoCall">
+     <div id="remote-video-container"></div>
+     <video id="local-video" autoPlay></video>
+     <button onClick={this.joinCall}>Join Call</button>
+     <button onClick={this.leaveCall}>Leave Call</button>
+    </div>)
+  }
+}
+export default VideoCall;
